e1e2dfdfc8b00f4f644161319aa15398f144ef65
sobuz80 <sobuz80@gmail.com>
2019-03-26 00:09:45 +0600

    initial commit

---
+++ CrudApplication/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudApplication.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Status Name")]
+        [StringLength(20, ErrorMessage = "The (0) Must be at least (2) characters long.", MinimumLength = 3)]
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Qty { get; set; }
+
+    }
+}
